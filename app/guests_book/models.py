f08c3a2de39fd50d01d3cc3909f5c5cc6e99d2f5
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-10-01 15:46:54 +0600

    Добавила в главную страницу форму добавления (Бонус +1)

--- app/guests_book/models.py
+++ app/guests_book/models.py
@@ -13,7 +13,7 @@ class Guests(models.Model):
     author = models.CharField(verbose_name='Имя автора', max_length=200, null=False, blank=False)
     email = models.EmailField(verbose_name="Почта автора", max_length=100, null=False, blank=False)
     description = models.CharField(verbose_name="Текст записи", max_length=2000, null=False, blank=False)
-    status = models.CharField(verbose_name="Статус", choices=StatusChoices.choices,default=StatusChoices.ACTIVE, max_length=50, null=False, blank=False)
+    status = models.CharField(verbose_name="Статус", choices=StatusChoices.choices, default=StatusChoices.ACTIVE, max_length=50, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
     changed_at = models.DateTimeField(verbose_name='Время редактирования', auto_now=True)
     is_deleted = models.BooleanField(verbose_name='Удалено', default=False, null=False)
--- app/guests_book/views/base.py
+++ app/guests_book/views/base.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.core.handlers.wsgi import WSGIRequest
 from guests_book.models import Guests
+from guests_book.forms import GuestForm
 
 
-def index_view(request: WSGIRequest):
-    guests = Guests.objects.filter(status='Active', is_deleted=False).order_by('-created_at')
-    context = {
-        'guests': guests
-    }
-    return render(request, 'index.html', context=context)
+def index_view(request):
+    if request.method == "GET":
+        guests = Guests.objects.filter(status='Active', is_deleted=False).order_by('-created_at')
+        form = GuestForm()
+        context = {
+            'guests': guests,
+            'form': form
+        }
+        return render(request, 'index.html', context=context)
