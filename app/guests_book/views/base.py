a185a8bd53ce74bef0c39af606f17efcd2d16ade
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-10-01 14:50:43 +0600

    Изменила конпки на старнице подтверждения удаления, поменяла немного стили в базовом шаблоне и главной

--- app/guests_book/views/base.py
+++ app/guests_book/views/base.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
-
 from guests_book.models import Guests
 
 
 def index_view(request: WSGIRequest):
-    guests = Guests.objects.filter(status='Active').order_by('-created_at')
+    guests = Guests.objects.filter(status='Active', is_deleted=False).order_by('-created_at')
     context = {
         'guests': guests
     }
